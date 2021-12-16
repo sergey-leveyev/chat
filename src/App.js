044a687afeff4bc78be6318541f85650c550d9a3
unknown <sargey7@gmail.com>
2021-12-16 15:07:20 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Button } from "components";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>hello</h1>
+      <Button type="primary" size="large">
+        hellooo
+      </Button>
+    </div>
+  );
+}
+
+export default App;
